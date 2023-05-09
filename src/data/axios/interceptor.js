399f446d07a1550f558ab0658293168fb9c5895d
divyashantkumarCS <divyashant@checkedspot.com>
2023-05-09 11:34:47 +0530

    09-05-2023

--- src/data/axios/interceptor.js
+++ src/data/axios/interceptor.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const http = axios.create({
-  baseURL: "http://localhost:8080",  
-  // baseURL: "https://apicheckedspot.azurewebsites.net",  
+  baseURL: "https://apicheckedspot.azurewebsites.net",  
   headers: {
     Accept: "application/json",
   },
