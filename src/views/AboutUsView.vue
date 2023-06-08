de14f192fedc49fdc220a7e2a24c6cb17ea5ee18
unknown <glvivek719@gmail.com>
2023-06-08 03:41:41 -0700

    upda

--- src/views/AboutUsView.vue
+++ src/views/AboutUsView.vue
@@ -1,10 +1,101 @@
 <template>
-    <div class="text-h4 font-weight-medium px-10 py-6">About Us</div>
+    <v-container fluid class="pa-0">
+        <div class="whoWeAreSec1 d-flex justify-center pb-0">
+        <v-img src="@/assets/images/pexels-photo-206359.jpeg"  height="100%" cover></v-img>
+        <h1 class="heading"><pre>ABOUT US</pre></h1>
+        </div>
+        <v-row no-gutters class="px-16 pt-5 pb-10">
+            <v-col cols="12" class="pb-8 mt-10">
+                <h1 class="font-weight-regular merriweatherFont1">
+                    Checked Spots are those properties which are not just those
+                    properties posted in web but they are,
+                </h1>
+            </v-col>
+            <v-col cols="12" md="6" class="pt-10 pr-5">
+                <v-img src="@/assets/images/save_Time.jpg"></v-img>
+            </v-col>
+            <v-col cols="12" md="6" class="pt-10 px-16 d-flex flex-column justify-center align-center">
+                <h1 class="merriweatherFont mb-8">Verified Properties: Save Time, Buy with Confidence!</h1>
+                <div class="d-flex align-center mb-3 pr-2">
+                    <p class="text-h6 font-weight-light">
+                        Properties are digitally and physically verified by one ore more
+                        of our checkedspot representative such that you can save your
+                        time and be sure on what you are buying.
+                    </p>
+                </div>
+            </v-col>
+        </v-row>
+        <v-row no-gutters class="px-16 pt-5 pb-10">
+
+            <v-col cols="12" md="6" class="pt-10 px-16 d-flex flex-column justify-center align-center">
+                <h1 class="merriweatherFont mb-8">Verified Documents: On-Site Verification with Legal Experts!</h1>
+                <div class="d-flex align-center mb-3 pr-2">
+                    <p class="text-h6 font-weight-light">
+                        Documents are verified by visiting the actual property owners
+                        along with our specialist legal team.
+                    </p>
+                </div>
+            </v-col>
+            <v-col cols="12" md="6" class="pt-10 pr-5">
+                <v-img src="@/assets/images/Verified_land.jpg"></v-img>
+            </v-col>
+        </v-row>
+
+        <v-row no-gutters class="px-16 py-5">
+            <v-col cols="12" md="6" class="pt-10 pr-5">
+                <v-img src="@/assets/images/warning.jpg"></v-img>
+            </v-col>
+            <v-col cols="12" sm="6" class="pt-10 px-16 d-flex flex-column justify-center align-center">
+                <h1 class="merriweatherFont mb-8">Swift Warning Flagging: Expert Team Identifies and Alerts Potential Issues!</h1>
+                <div class="d-flex align-center mb-3 pr-2">
+                    <p class="text-h6 font-weight-light">
+                        Any thing which our expert team finds as a warning its
+                        immediately flagged.
+                    </p>
+                </div>
+            </v-col>
+        </v-row>
+        <v-row no-gutters class="px-8 pt-5">
+            <v-col cols="12">
+                <p class="my-4 text-h6 font-weight-light pl-8">
+                To see a sample report on each of property listed is verified who
+                can click here to see a
+                <a href="#" class="text-blue-darken-4">Sample Report - Checked Spot</a>
+
+                <br />
+
+                Otherwise, you're done!
+                </p>
+            </v-col>
+        </v-row>
+    </v-container>
+    
 </template>
 
 <script lang="ts" setup>
 </script>
 
-<style>
+<style scoped>
+  /* @import url('https://fonts.googleapis.com/css2?family=EB+Garamond:wght@700&display=swap'); */
+  @import url('https://fonts.googleapis.com/css2?family=Merriweather:ital,wght@0,300;0,400;0,700;0,900;1,300;1,400;1,700;1,900&display=swap');
+    .whoWeAreSec1{
+        position: relative;
+        width: 100%;
+        height: 400px;
+    }
+    .heading{
+        position: absolute;
+        top: 100px;
+        color: white;
+        font-size: 120px;
+    }
+
+    .merriweatherFont1{
+        font-family: 'Merriweather', serif !important;
+        font-size: 45px;
+    }
+    .merriweatherFont{
+        font-family: 'Merriweather', serif !important;
+    }
 
 </style>
