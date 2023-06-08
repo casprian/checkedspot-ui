2ff4c89953db4b13f85f57f9ba7ec2cf32a9e4d6
unknown <glvivek719@gmail.com>
2023-06-08 06:26:55 -0700

    up

--- src/views/WhatWeDoView.vue
+++ src/views/WhatWeDoView.vue
@@ -1,112 +1,117 @@
 <template>
-    <v-container fluid class="pa-0">
-        <!-- Company logo and header -->
-        <v-row no-gutters class="whatwedoHeader">
-            <v-col cols="12">
-                <v-img src="@/assets/logocheckedspot.png" contain></v-img>
-            </v-col>
-        </v-row>
+    <v-row no-gutters>
+        <v-col cols="12">
+            <v-row no-gutters class="pa-4 pa-sm-8 px-md-16 py-md-8 mx-0 mx-md-16">
+                <v-col class="pa-4 px-md-16 py-md-8 text-center mx-0 mx-md-16">
+                    <h1 class="merriweatherFont pb-3">What we do</h1>
+                    <p class="text-h6 font-weight-light">We generate value for our clients across all facets of their real estate. We work alongside them to build new capabilities, transform business models, convert disruption into opportunity, and create outsized growth while unlocking greater economic outcomes and societal returns for our communities.</p>
+                </v-col>
+            </v-row>
+        </v-col>
+        <v-col cols="12">
+            <v-row no-gutters>
+                <v-col cols="12" md="6">
+                    <v-img src="@/assets/images/whatWeDo1.jpg" height="400" cover></v-img>
+                </v-col>
+                <v-col cols="12" md="6" class="py-10 py-md-0 px-10 px-md-16 d-flex flex-column justify-center align-center">
+                    <h1 class="merriweatherFont pb-3 px-md-7">Transform spaces to be fit-for-purpose</h1>
+                    <p class="text-h6 font-weight-light px-md-7">We collaborate with organizations across all industries to reimagine and transform real estate from a cost center to a competitive advantage by delivering spaces, technology, and experiences that unlock heightened productivity and delight employees and customers.</p>
+                </v-col>
+            </v-row>
+        </v-col>
+        <v-col cols="12" md="8" class="my-6 my-md-16 mx-md-9 px-10 px-md-16">
+            <p class="text-body-2">Example of transforming a client's space to be fit-for-purpose</p>
+            <h3 class="merriweatherSmallFont mb-4">A new flexible working model for a financial services firm</h3>
+            <p class="text-h6 font-weight-light pr-md-6" >Collaborated with a major US financial services firm to design a new flexible working model in response to COVID-19. Engaging leaders across functions, including the full executive team, we transitioned the company from less than 20 percent of employees engaged in virtual working models to over 80 percent fully virtual or flexible. We reduced real estate costs by more than 60 percent, while moving the company’s organizational health into the top decile.</p>
+        </v-col>
+        <v-col cols="12" class="mb-10 mb-md-0">
+            <v-row no-gutters>
+                <v-col cols="12" md="6" class="py-8 py-md-0 px-10 px-md-16 d-flex flex-column justify-center">
+                    <h1 class="merriweatherFont pb-3 px-md-7">Lead through climate</h1>
+                    <p class="text-h6 font-weight-light px-md-7">We equip clients to incorporate climate into portfolio valuations, decarbonize the built environment, achieve new frontiers in green development, and create new sources of value for investors, tenants, and communities.</p>
+                </v-col>
+                <v-col cols="12" md="6">
+                    <v-img src="@/assets/images/climate.jpg" height="400" cover></v-img>
+                </v-col>
+            </v-row>
+        </v-col>
+        <v-col cols="12" md="8" class="my-6 mb-sm-10 my-md-16 mx-md-9 px-10 px-md-16">
+            <p class="text-body-2">Example of achieving impact through climate</p>
+            <h3 class="merriweatherSmallFont mb-4">Sustainability for a leading financial institution</h3>
+            <p class="text-h6 font-weight-light pr-md-6">Unearthed material climate risks across a $40 billion portfolio that threatened 80 percent of profits and built an enduring capability for a client, including climate-driven advanced analytics for pricing and new offerings for counterparties to address climate risks in their properties.</p>
+        </v-col>
+        <v-col cols="12">
+            <v-row no-gutters>
+                <v-col cols="12" md="6">
+                    <v-img src="@/assets/images/whatWeDo1.jpg" max-height="450" cover></v-img>
+                </v-col>
+                <v-col cols="12" md="6" class="py-10 py-md-0 px-10 px-md-16 d-flex flex-column justify-center align-center">
+                    <h1 class="merriweatherFont pb-3 px-md-7">Differentiate through digital and advanced analytics</h1>
+                    <p class="text-h6 font-weight-light px-md-7">We empower clients to transform their real estate organizations by digitizing the experience of the space and improving decision making through advanced analytics. In doing so, we create more efficient organizations, improve real estate outcomes, and unlock additional sources of revenue while building powerful digital ecosystems and data assets.</p>
+                </v-col>
+                
+            </v-row>
+        </v-col>
+        <v-col cols="12" md="8" class="my-6 mb-sm-10 my-md-16 mx-md-9 px-10 px-md-16">
+            <p class="text-body-2">Example of reimagining the tenant experience</p>
+            <h3 class="merriweatherSmallFont mb-4">Digital transformation and new business building at RXR Realty</h3>
+            <p class="text-h6 font-weight-light pr-md-6">We helped RXR Realty, a North American real estate investor and developer, reimagine the tenant experience across its residential, commercial, and mixed-use properties. We established a lab of more than 25 data scientists, designers, engineers, and product managers, who worked together to develop digital capabilities that catalyzed the reinvention of the company, helping it emerge as a beacon of change for the industry. Through our partnership, the company has built proprietary digital capabilities which are unique in the real estate industry, which has enabled the organization to move with speed and agility.</p>
+        </v-col>
+        <v-col cols="12" class="mb-10 mb-md-0">
+            <v-row no-gutters>
+                <v-col cols="12" md="6" class="py-8 py-md-0 px-10 px-md-16 d-flex flex-column  justify-center align-center">
+                    <h1 class="merriweatherFont pb-3 px-md-7">Unlock demand in developments</h1>
+                    <p class="text-h6 font-weight-light px-md-7">We partner with clients to define, deliver, and fulfill the potential of marquee developments by generating momentum and creating organic demand. By helping clients achieve the mission of “placemakers,” we create ecosystems that outperform economically and shape more equitable and dynamic communities.</p>
+                </v-col>
+                <v-col cols="12" md="6">
+                    <v-img src="@/assets/images/climate.jpg" height="450" cover></v-img>
+                </v-col>
+            </v-row>
+        </v-col>
+        <v-col cols="12" md="8" class="my-6 mb-sm-10 my-md-16 mb-md-8 mx-md-9 px-10 px-md-16">
+            <p class="text-body-2">Example of helping a client unlock demand in a development</p>
+            <h3 class="merriweatherSmallFont mb-4">Repositioning of district-sized development in the United States</h3>
+            <p class="text-h6 font-weight-light pr-md-16">Generated $3 billion in asset value through repositioning a 15-million square foot development into a curated value proposition for anchor tenants, a proactive tenant attraction approach, new revenue streams through infrastructure, and exceptional experiences for how users of space want to engage in five years.</p>
+        </v-col>
+        <v-col cols="12" md="8" class="my-6 mb-sm-10 my-md-8 mb-md-16 mx-md-9 px-10 px-md-16 pb-8 pb-md-0">
+            <p class="text-body-2">Example of achieving the mission of placemaker</p>
+            <h3 class="merriweatherSmallFont mb-4">Driving delight and visitor demand in an emerging tourist destination</h3>
+            <p class="text-h6 font-weight-light pr-md-16">Collaborated with a regional government to ideate and execute innovative experiences for 2 million target visitors. Using a design thinking led approach, we explored more than 500 new visitor experiences and executed 15 of them, detailed with market assessment, feasibility, financial model, and customer journeys of each target persona. We also supported the approval of an investment framework that enabled a $1 billion agreement with multiple leading hotel properties.</p>
+        </v-col>
+        <v-col cols="12">
+            <v-row no-gutters>
+                <v-col cols="12" md="6">
+                    <v-img src="@/assets/images/whatWeDo1.jpg" max-height="450" cover></v-img>
+                </v-col>
+                <v-col cols="12" md="6" class="py-10 py-md-0 px-10 px-md-16 d-flex flex-column justify-center align-center">
+                    <h1 class="merriweatherFont pb-3 px-md-7">Drive value through operations</h1>
+                    <p class="text-h6 font-weight-light px-md-7">Competitive differentiation no longer hinges upon transactions alone; today, exceptionalism in operations is critical to outpace the competition. We embolden clients to create value through operational excellence with better models to serve, strengthened capabilities, and engines for sustained growth.</p>
+                </v-col>
+            </v-row>
+        </v-col>
+        <v-col cols="12" md="8" class="my-6 mt-3 mt-md-16 mx-md-9 px-10 px-md-16 pb-8 pb-md-0">
+            <p class="text-body-2">Example of creating value through operational excellence</p>
+            <h3 class="merriweatherSmallFont mb-4">Performance transformation of a retail platform</h3>
+            <p class="text-h6 font-weight-light pr-md-6">Delivered on a holistic transformation of a regional shopping mall real estate investment trust to capture 15 to 25 percent net-operating income uplift by pivoting the strategy, redesigning the organization, digitizing core processes, injecting data insights and analytics, and building cross-portfolio centers of excellence in sales operations, marketing, and procurement.</p>
+        </v-col>
+    </v-row>
 
-        <!-- Main content -->
-        <v-row>
-            <v-col cols="12">
-                <!-- Section: Our Services -->
-                <v-card>
-                    <v-card-title>Our Services</v-card-title>
-                    <v-card-text>
-                        <p>
-                            At XYZ Real Estate, we provide a comprehensive range of services to help you with all your real
-                            estate needs. With our expert team and personalized approach, we are committed to delivering
-                            exceptional results and making your real estate journey smooth and successful.
-                        </p>
-                        <ul>
-                            <li>
-                                <strong>Residential Property Sales and Purchases:</strong> Whether you're buying or selling
-                                a home, our experienced agents will guide you through the process, ensuring you find the
-                                perfect property or secure the best deal for your home.
-                            </li>
-                            <li>
-                                <strong>Commercial Property Sales and Leasing:</strong> Our commercial real estate experts
-                                specialize in helping businesses find the ideal commercial space and negotiate favorable
-                                lease terms. We also assist in selling commercial properties to maximize returns.
-                            </li>
-                            <li>
-                                <strong>Property Management and Rentals:</strong> For property owners, we offer professional
-                                property management services, including tenant screening, rent collection, and maintenance
-                                coordination. We also help tenants find quality rental properties that meet their needs and
-                                budget.
-                            </li>
-                            <li>
-                                <strong>Investment Property Consulting:</strong> Looking to invest in real estate? Our
-                                investment property consultants provide expert advice and market insights to help you make
-                                informed decisions and achieve your investment goals.
-                            </li>
-                            <li>
-                                <strong>Real Estate Valuation and Appraisal:</strong> Our certified appraisers provide
-                                accurate and detailed property valuations for various purposes, such as mortgage financing,
-                                estate planning, or property tax assessments.
-                            </li>
-                            <li>
-                                <strong>Real Estate Financing Assistance:</strong> We have partnerships with reputable
-                                lenders and mortgage brokers who can help you secure the best financing options for your
-                                real estate transactions, saving you time and money.
-                            </li>
-                        </ul>
-                    </v-card-text>
-                </v-card>
-
-                <!-- Section: Why Choose Us -->
-                <v-card>
-                    <v-card-title>Why Choose Us</v-card-title>
-                    <v-card-text>
-                        <p>
-                            At XYZ Real Estate, we stand out from the competition. Here's why you should choose us as your
-                            trusted real estate partner:
-                        </p>
-                        <ul>
-                            <li>
-                                <strong>Experience and Expertise:</strong> With over 20 years in the industry, our team of
-                                licensed professionals has extensive knowledge of the local real estate market, trends, and
-                                regulations. We leverage this expertise to provide you with the best possible guidance and
-                                results.
-                            </li>
-                            <li>
-                                <strong>Personalized Approach:</strong> We understand that every client is unique, and we
-                                take the time to listen and understand your specific requirements. Our tailored approach
-                                ensures that we address your individual needs and deliver customized solutions.
-                            </li>
-                            <li>
-                                <strong>Attention to Detail:</strong> We believe in meticulous attention to detail. From
-                                property research and market analysis to contract negotiations and paperwork, we handle
-                                every aspect of the real estate process with utmost care and precision.
-                            </li>
-                            <li>
-                                <strong>Transparent Communication:</strong> We believe in open and transparent communication
-                                throughout the entire process. We keep you informed at every step, provide regular updates,
-                                and promptly address any questions or concerns you may have.
-                            </li>
-                            <li>
-                                <strong>Exceptional Customer Service:</strong> Our dedication to customer satisfaction is
-                                unmatched. We go above and beyond to ensure that your real estate experience with us is
-                                smooth, stress-free, and exceeds your expectations.
-                            </li>
-                        </ul>
-                    </v-card-text>
-                </v-card>
-            </v-col>
-        </v-row>
-    </v-container>
+        <v-row></v-row>
 </template>
-  
-<script lang="ts" setup>
+
+<script setup>
 
 </script>
-  
+
 <style scoped>
-/* Add custom CSS styles here */
-.whatwedoHeader {
-    background-image: linear-gradient(90deg, rgba(254,254,254,0.5) 8%, rgba(255,255,255,0.5) 87%), url('@/assets/images/welcome.jpg');
-    background-size: cover;
-}
+    @import url('https://fonts.googleapis.com/css2?family=Merriweather:ital,wght@0,300;0,400;0,700;0,900;1,300;1,400;1,700;1,900&display=swap');
+
+    .merriweatherFont{
+        font-family: 'Merriweather', serif !important;
+        font-size: 45px;
+    }
+    .merriweatherSmallFont{
+        font-family: 'Merriweather', serif !important;
+        font-size: 26px;
+    }
 </style>
-  
