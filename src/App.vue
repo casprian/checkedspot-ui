eb8b52e03651b88706b4d6cc2b0a365ab273edd2
divyashantkumarCS <divyashant@checkedspot.com>
2023-06-13 12:16:38 +0530

    13-06-2023

--- src/App.vue
+++ src/App.vue
@@ -4,3 +4,15 @@
 
 <script lang="ts" setup>
 </script>
+
+<style>
+::-moz-selection { /* Code for Firefox */
+  color: #fff;
+  background: #f5005696;
+}
+
+::selection {
+  color: #fff;
+  background: #f5005696;
+}
+</style>
