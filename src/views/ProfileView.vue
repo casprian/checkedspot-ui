dfb10a608e26dc5ec542085efb513df8146b1e3e
divyashantkumarCS <divyashant@checkedspot.com>
2023-06-15 10:25:37 +0530

    15-06-2023

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -5,33 +5,45 @@
         <v-row no-gutters>
           <v-col cols="12" class="d-flex align-center justify-center">
             <v-avatar color="grey-darken-3"
-              image='https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairShortCurly&accessoriesType=Prescription02&hairColor=Black&facialHairType=Blank&clotheType=Hoodie&clotheColor=White&eyeType=Default&eyebrowType=DefaultNatural&mouthType=Default&skinColor=Light'
-              size="170"
-            ></v-avatar>
+              :image="userdata?.picture ? userdata?.picture : 'https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairShortCurly&accessoriesType=Prescription02&hairColor=Black&facialHairType=Blank&clotheType=Hoodie&clotheColor=White&eyeType=Default&eyebrowType=DefaultNatural&mouthType=Default&skinColor=Light'"
+              size="170"></v-avatar>
           </v-col>
           <v-col cols="12" class="pt-3 myProfileDetails">
-            <h2 class="py-3"><div>Name</div> : <span>Vivek GL</span></h2>
-            <h2 class="py-3"><div>Email</div> : <span>vivek@gmail.com</span></h2>
-            <h2 class="py-3"><div>Contact</div> : <span>9100496701</span></h2>
-            <h2 class="py-3"><div>Adress</div> : <span>Hegde Nagar, Bengaluru</span></h2>
-            <h2 class="py-3"><div>City</div> : <span>Bengaluru</span></h2>
-            <h2 class="py-3"><div>State</div> : <span>Karnataka</span></h2>
+            <h2 class="py-3">
+              <div>Name</div> : <span>Vivek GL</span>
+            </h2>
+            <h2 class="py-3">
+              <div>Email</div> : <span>vivek@gmail.com</span>
+            </h2>
+            <h2 class="py-3">
+              <div>Contact</div> : <span>9100496701</span>
+            </h2>
+            <h2 class="py-3">
+              <div>Adress</div> : <span>Hegde Nagar, Bengaluru</span>
+            </h2>
+            <h2 class="py-3">
+              <div>City</div> : <span>Bengaluru</span>
+            </h2>
+            <h2 class="py-3">
+              <div>State</div> : <span>Karnataka</span>
+            </h2>
           </v-col>
           <v-col class="mt-4">
-            <v-btn prepend-icon="mdi-pencil" class="d-md-none rounded-lg" variant="outlined" @click="showEdit=!showEdit">Edit</v-btn>
+            <v-btn prepend-icon="mdi-pencil" class="d-md-none rounded-lg" variant="outlined"
+              @click="showEdit = !showEdit">Edit</v-btn>
           </v-col>
         </v-row>
-        
+
       </v-col>
-      <v-col cols="12" md="6" class="elevation-2 pa-10 d-none d-md-block" :class="[showEdit?'d-none d-md-block':'d-block']">
+      <v-col cols="12" md="6" class="elevation-2 pa-10 d-none d-md-block"
+        :class="[showEdit ? 'd-none d-md-block' : 'd-block']">
         <v-form>
           <v-row no-gutters>
             <v-col cols="12" class="d-flex justify-space-between align-center mb-10">
               <h1>Edit Profile</h1>
               <v-avatar color="grey-darken-3"
-              image='https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairShortCurly&accessoriesType=Prescription02&hairColor=Black&facialHairType=Blank&clotheType=Hoodie&clotheColor=White&eyeType=Default&eyebrowType=DefaultNatural&mouthType=Default&skinColor=Light'
-              size="70"
-              ></v-avatar>
+                :image="user?.picture ? user?.picture : 'https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairShortCurly&accessoriesType=Prescription02&hairColor=Black&facialHairType=Blank&clotheType=Hoodie&clotheColor=White&eyeType=Default&eyebrowType=DefaultNatural&mouthType=Default&skinColor=Light'"
+                size="70"></v-avatar>
             </v-col>
             <v-col cols="12">
               <div class="text-body-1 font-weight-medium">Name</div>
@@ -65,33 +77,71 @@
       </v-col>
     </v-row>
   </v-container>
-  
 </template>
 
 <script lang="ts" setup>
+//@ts-ignore
+import api from '@/data/api/index.js';
+import jwtDecode from 'jwt-decode';
+import { onMounted, reactive, ref, watch } from 'vue'
+import { useCookies } from 'vue3-cookies';
 
-  import {reactive,ref} from 'vue'
+const Stateitems = reactive(['Karnataka']);
+const countryitems = reactive(['India']);
+const showEdit = ref(false);
+const { cookies } = useCookies();
 
-  const Stateitems = reactive(['Karnataka']);
-  const countryitems = reactive(['India']);
-  const showEdit = ref(false)
+let userdata = reactive({
+  name: null,
+  picture: null,
+  email: null,
+  contact: null,
+  adress: null,
+  city: null,
+  state: null
+});
 
+watch(userdata, (newuserdata) => {
+  console.log(newuserdata)
+  return newuserdata
+},{ deep: true, immediate: true })
+
+let user = reactive({
+  name: null,
+  email: null,
+  picture: null
+});
+
+if (cookies.get('token')) {
+  //@ts-ignore
+  user = jwtDecode(cookies.get('token'))?.userData;
+}
+
+async function getUser() {
+  const res = await api?.user?.getUserData({ params: { email: user?.email } });
+  userdata = res?.data?.data;
+  console.log(userdata)
+}
+
+onMounted(async () => {
+  await getUser();
+})
 </script>
 
 <style scoped>
+.myProfileDetails>h2 {
+  font-size: 25px;
+  font-weight: 300;
+}
 
-.myProfileDetails > h2 {
-    font-size: 25px;
-    font-weight: 300;
-  }
-  .myProfileDetails > h2 > div{
-    display: inline-block;
-    width: 120px;
-    font-size: 30px;
-    font-weight: 400;
-  }
+.myProfileDetails>h2>div {
+  display: inline-block;
+  width: 120px;
+  font-size: 30px;
+  font-weight: 400;
+}
 
-  .myProfileDetails > h2 > span{
-    margin-left:10px;
-  }
+.myProfileDetails>h2>span {
+  margin-left: 10px;
+}
 </style>
