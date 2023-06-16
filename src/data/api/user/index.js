0883bd22c4c510357ac1ecba8b489adabcbfd6bf
divyashantkumarCS <divyashant@checkedspot.com>
2023-06-16 18:33:29 +0530

    16-06-2023

--- src/data/api/user/index.js
+++ src/data/api/user/index.js
@@ -15,7 +15,6 @@ const login = async (params) => {
     try {
         const URL = "/user/login";
         const response = await call.callWithoutToken('post',URL,params)
-        console.log("response: ", response)
         return {status: 200, data: response};
     }catch(e) {
         return {error: true, status: e?.response?.status, message: e?.response?.data?.message};
