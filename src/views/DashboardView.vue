8ee11c5a36f28cb756139361b851cb17206d8367
divyashantkumarCS <divyashant@checkedspot.com>
2023-07-03 18:09:18 +0530

    updated

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -4,11 +4,10 @@
             <v-col cols="12" sm="3" class="bg-grey-lighten-5 leftNavigation">
                 <v-sheet height="85" class="pl-5 d-flex align-center bg-transparent">
                     <div class="d-flex">
-                        <v-img 
-                            width="30"
-                            src="https://checkedspot.blob.core.windows.net/assets/checkedspotfavicon.ico"
-                        ></v-img>
-                        <h2 class="ml-3 mt-2 mt-md-1 mr-3 text-body-1 text-md-h5 font-weight-medium text-grey-darken-3">User Dashboard</h2>
+                        <v-img width="30"
+                            src="https://checkedspot.blob.core.windows.net/assets/checkedspotfavicon.ico"></v-img>
+                        <h2 class="ml-3 mt-2 mt-md-1 mr-3 text-body-1 text-md-h5 font-weight-medium text-grey-darken-3">User
+                            Dashboard</h2>
                     </div>
                 </v-sheet>
                 <v-divider></v-divider>
@@ -21,26 +20,14 @@
                 <v-btn block variant="text" class="my-2 px-8 d-flex justify-start align-center" height="45"
                     prepend-icon="mdi-home-city-outline" color="grey-darken-3"
                     @click="router.push('/dashboard/properties')">Properties</v-btn>
-
-
-                <v-btn block variant="text" class="my-2 px-8 d-flex justify-start align-center" height="45"
-                    prepend-icon="mdi-cube-outline" color="grey-darken-3">Projects</v-btn>
-
-
-                <v-btn block variant="text" class="my-2 px-8 d-flex justify-start align-center" height="45"
-                    prepend-icon="mdi-face-agent" color="grey-darken-3">Support</v-btn>
-
-
-                <v-btn block variant="text" class="my-2 px-8 d-flex justify-start align-center" height="45"
-                    prepend-icon="mdi-room-service-outline" color="grey-darken-3">Services</v-btn>
-
             </v-col>
 
             <v-col cols="12" sm="9" class="bg-grey-lighten-3">
                 <v-row no-gutters>
-                    <v-col cols="12" class="d-flex justify-space-between align-center bg-grey-lighten-5 rightTopNaigation pt-2 px-5">
+                    <v-col cols="12"
+                        class="d-flex justify-space-between align-center bg-grey-lighten-5 rightTopNaigation pt-2 px-5">
                         <v-btn @click="router.push('/')" variant="outlined" prepend-icon="mdi-home">Home</v-btn>
-                        <profile-avatar/>
+                        <profile-avatar />
                     </v-col>
 
                     <v-col cols="12" class="px-1 d-flex justify-center rightBottomNaigation">
@@ -64,10 +51,10 @@ const router = useRouter();
 const user = ref();
 const token = ref(false)
 if (cookies.get('token')) {
-  //@ts-ignore
-  user.value = jwtDecode(cookies.get('token'))?.userData;
-  token.value = true;
-}else {
+    //@ts-ignore
+    user.value = jwtDecode(cookies.get('token'))?.userData;
+    token.value = true;
+} else {
     router.push("/");
 }
 </script>
@@ -84,8 +71,9 @@ if (cookies.get('token')) {
     height: 60px;
     box-shadow: 0 0px 6px 0 rgba(0, 0, 0, 0.3);
 }
+
 .rightBottomNaigation {
-    height: calc(100vh - 60px); 
+    height: calc(100vh - 60px);
     overflow-y: scroll;
 }
 </style>
